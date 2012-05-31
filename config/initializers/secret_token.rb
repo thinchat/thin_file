7afad7733a073826edbef7c728f058080dc15351
Chris Anderson <christopher.anderson@gmail.com>
2012-05-31 15:18:29 -0400

    initial app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinFile::Application.config.secret_token = 'b50f7d880bf42a106f142182fcc9951290eadff381d96f83c55293e7a86dc77108a006c0ff6aa3b18c8c90d3800b4af422fe0555935b33c601fa76d9bc769d36'
