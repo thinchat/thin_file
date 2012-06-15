6b54c529923c1756ab6dcd8e3a0ae3d4ab706e41
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-15 14:32:54 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ require 'api_constraints'
 ThinFile::Application.routes.draw do
   scope '/file' do
     root :to => "home#index"
-  end
 
-  resources :pictures
-  resources :documents
+    resources :pictures
+    resources :documents
 
-  namespace :api, defaults: {format: 'json'} do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :pictures
-      resources :documents
+    namespace :api, defaults: {format: 'json'} do
+      namespace :v1
+        resources :pictures
+        resources :documents
+      end
     end
   end
 end
